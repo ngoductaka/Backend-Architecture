f9c50d7faefdbbc18b56f5ed8aea8ac4e390a40e
Loitd <trandailoi140201@gmail.com>
2026-03-05 16:36:04 +0700

    update schema 15

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -4,93 +4,130 @@ const DOCUMENT_NAME = "Product";
 const COLLECTION_NAME = "Products";
 
 const productSchema = new Schema({
-    product_name: {
-        type: String,
-        required: true,
-    },
-    product_thumb: {
-        type: String,
-    },
-    product_description: {
-        type: String,
-    },
-    product_price: {
-        type: Number,
-    },
-    product_quantity: {
-        type: Number,
-    },
-    product_type: {
-        type: String,
-        enum: ['electronics', 'clothing', 'furniture', 'beauty', 'sports', 'toys', 'other'],
-    },
-    product_attributes: {
-        type: Schema.Types.Mixed,
-    },
-    product_shop: {
-        type: Schema.Types.ObjectId,
-        ref: 'Shop',
-    },
-    // ===========================
-
-
-    product_category: {
-        type: String,
-    },
-    product_brand: {
-        type: String,
-    },
-    product_images: {
-        type: [String],
-    },
-    product_videos: {
-        type: [String],
-    },
-    product_reviews: {
-        type: [String],
-    },
-    product_ratings: {
-        type: [Number],
-    },
-    product_tags: {
-        type: [String],
-    },
-    product_variants: {
-        type: [String],
-    },
-    product_options: {
-        type: [String],
-    },
-    product_variants: {
-        type: [String],
-    },
-})
+  product_name: {
+    type: String,
+    required: true,
+  },
+  product_slug: {
+    type: String,
+  },
+  product_thumb: {
+    type: String,
+  },
+  product_description: {
+    type: String,
+  },
+  product_price: {
+    type: Number,
+  },
+  product_quantity: {
+    type: Number,
+  },
+  product_type: {
+    type: String,
+    enum: [
+      "electronics",
+      "clothing",
+      "furniture",
+      "beauty",
+      "sports",
+      "toys",
+      "other",
+    ],
+  },
+  product_attributes: {
+    type: Schema.Types.Mixed,
+  },
+  product_shop: {
+    type: Schema.Types.ObjectId,
+    ref: "Shop",
+  },
+  product_ratingsAverage: {
+    type: Number,
+    default: 4.5,
+    min: [1, `Rating must be above 1.0`],
+    max: [5, `Rating must be below 5.0`],
+    set: (val) => Math.round(val * 10) / 10, // Round to 1 decimal place
+  },
+  product_variants: {
+    type: [String],
+  },
+  isDraft: {
+    type: Boolean,
+    default: true,
+    index: true,
+    select: false,
+  },
+  isPublished: {
+    type: Boolean,
+    default: false,
+    index: true,
+    select: false,
+  },
+  product_category: {
+    type: String,
+  },
+  product_brand: {
+    type: String,
+  },
+  product_images: {
+    type: [String],
+  },
+  product_videos: {
+    type: [String],
+  },
+  product_reviews: {
+    type: [String],
+  },
+  product_ratings: {
+    type: [Number],
+  },
+  product_tags: {
+    type: [String],
+  },
+  product_variants: {
+    type: [String],
+  },
+  product_options: {
+    type: [String],
+  },
+  product_variants: {
+    type: [String],
+  },
+});
+productSchema.pre("save", function (next) {
+  this.product_slug = this.product_name
+    .toLowerCase()
+    .replace(/[^a-z0-9]+/g, "-") // Replace non-alphanumeric characters with hyphens
+    .replace(/^-+|-+$/g, "");
+  next();
+});
 
 const clothingSchema = new Schema({
-    brand: {
-        type: String,
-    },
-    size: {
-        type: String,
-    },
-    color: {
-        type: String,
-    },
-})
+  brand: {
+    type: String,
+  },
+  size: {
+    type: String,
+  },
+  color: {
+    type: String,
+  },
+});
 
 const electronicsSchema = new Schema({
-    brand: {
-        type: String,
-    },
-    model: {
-        type: String,
-    },
-    color: {
-        type: String,
-    },
-})
+  brand: {
+    type: String,
+  },
+  model: {
+    type: String,
+  },
+  color: {
+    type: String,
+  },
+});
 
 export default model(DOCUMENT_NAME, productSchema);
-export const electronics = model('Electronics', electronicsSchema);
-export const clothing = model('Clothing', clothingSchema);
+export const electronics = model("Electronics", electronicsSchema);
+export const clothing = model("Clothing", clothingSchema);
 // Polymorphic pattern
--- src/services/product.service.js
+++ src/services/product.service.js
@@ -1,54 +1,86 @@
-import product, { clothing, electronics } from '../models/product.model.js';
-import { SuccessResponse } from '../core/success.response.js';
+import product, { clothing, electronics } from "../models/product.model.js";
+import { SuccessResponse } from "../core/success.response.js";
+import { ErrorResponse } from "../core/error.response.js";
 
 class ProductFactory {
-    static createProduct(type, data) {
-        console.log("Creating product with type:", type, "and data:", data);
-        switch (type) {
-            case 'clothing':
-                return new Clothing(data).createProduct();
-            case 'electronics':
-                return new Electronics(data).createProduct();
-            default:
-                throw new SuccessResponse('Invalid product type', 400);
-        }
+  static productRegistry = {};
+
+  static registerProductType(type, classRef) {
+    this.productRegistry[type] = classRef;
+  }
+  static createProduct(type, data) {
+    console.log("Creating product with type:", type, "and data:", data);
+    const ProductClass = this.productRegistry[type];
+    if (!ProductClass) {
+      throw new SuccessResponse("Invalid product type", 400);
     }
+    return new ProductClass(data).createProduct();
+  }
 }
 export class Product {
-    constructor({ product_shop, product_attributes, product_name = '', product_thumb, product_price = 0, product_quantity, product_type, product_description = '' }) {
-        this.product_shop = product_shop;
-        this.product_name = product_name;
-        this.product_thumb = product_thumb;
-        this.product_price = product_price;
-        this.product_quantity = product_quantity;
-        this.product_type = product_type;
-        this.product_description = product_description;
-        this.product_attributes = product_attributes;
-    }
-    async createProduct(product_id) {
-        return await product.create({...this, _id: product_id});
-    }
+  constructor({
+    product_shop,
+    product_attributes,
+    product_name = "",
+    product_thumb,
+    product_price = 0,
+    product_quantity,
+    product_type,
+    product_description = "",
+  }) {
+    this.product_shop = product_shop;
+    this.product_name = product_name;
+    this.product_thumb = product_thumb;
+    this.product_price = product_price;
+    this.product_quantity = product_quantity;
+    this.product_type = product_type;
+    this.product_description = product_description;
+    this.product_attributes = product_attributes;
+  }
+  async createProduct(product_id) {
+    return await product.create({ ...this, _id: product_id });
+  }
 }
 
 export class Clothing extends Product {
-    async createProduct() {
-        const newClothing = await clothing.create(this.product_attributes);
-        if(!newClothing) throw new SuccessResponse('Failed to create clothing attributes', 400);
-        const newProduct = await super.createProduct(newClothing._id);
-        if(!newProduct) throw new SuccessResponse('Failed to create newProduct attributes', 400);
+  async createProduct() {
+    const newClothing = await clothing.create(this.product_attributes);
+    if (!newClothing)
+      throw new ErrorResponse("Failed to create clothing attributes", 400);
+    const newProduct = await super.createProduct(newClothing._id);
+    if (!newProduct)
+      throw new ErrorResponse("Failed to create newProduct attributes", 400);
 
-        return newProduct;
-    }
+    return newProduct;
+  }
 }
 
 export class Electronics extends Product {
-    async createProduct() {
-        const newElectronics = await electronics.create(this.product_attributes);
-        if(!newElectronics) throw new SuccessResponse('Failed to create electronics attributes', 400);
-        const newProduct = await super.createProduct(newElectronics._id);
-        if(!newProduct) throw new SuccessResponse('Failed to create newProduct attributes', 400);
+  async createProduct() {
+    const newElectronics = await electronics.create(this.product_attributes);
+    if (!newElectronics)
+      throw new ErrorResponse("Failed to create electronics attributes", 400);
+    const newProduct = await super.createProduct(newElectronics._id);
+    if (!newProduct)
+      throw new ErrorResponse("Failed to create newProduct attributes", 400);
 
-        return newProduct;
-    }
+    return newProduct;
+  }
 }
+export class Furniture extends Product {
+  async createProduct() {
+    const newFurniture = await furniture.create(this.product_attributes);
+    if (!newFurniture)
+      throw new ErrorResponse("Failed to create furniture attributes", 400);
+    const newProduct = await super.createProduct(newFurniture._id);
+    if (!newProduct)
+      throw new ErrorResponse("Failed to create newProduct attributes", 400);
+
+    return newProduct;
+  }
+}
+// register product types
+ProductFactory.registerProductType("clothing", Clothing);
+ProductFactory.registerProductType("electronics", Electronics);
+ProductFactory.registerProductType("furniture", Furniture);
 export default ProductFactory;
